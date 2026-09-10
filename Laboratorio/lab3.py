715abd14ac5787d62db39e99df24b85287c12ea0
brandonmartinez050218-ops <brandonmartinez050218@gmail.com>
2026-09-10 12:04:56 -0500

    Update lab3.py

--- Laboratorio/lab3.py
+++ Laboratorio/lab3.py
@@ -1,1 +1,19 @@
+from machine import Pin, ADC
+from time import sleep
 
+led = Pin(2, Pin.OUT)
+pot = ADC(Pin(34))
+
+pot.atten(ADC.ATTN_11DB)
+pot.width(ADC.WIDTH_12BIT)
+
+while True:
+    valor = pot.read()
+    print(valor)
+
+    if 1800 <= valor <= 2300:
+        led.on()
+    else:
+        led.off()
+
+    sleep(0.1)
