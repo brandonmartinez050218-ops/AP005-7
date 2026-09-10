08f0685447a52be9beabe8f5142d2736cdd5d599
brandonmartinez050218-ops <brandonmartinez050218@gmail.com>
2026-09-10 11:59:26 -0500

    Update lab1.py

--- Laboratorio/lab1.py
+++ Laboratorio/lab1.py
@@ -1,1 +1,11 @@
-En esta carpeta estarán los códigos de las practicas realizadas en laboratorio
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.on()
+    sleep(1)
+
+    led.off()
+    sleep(1)
